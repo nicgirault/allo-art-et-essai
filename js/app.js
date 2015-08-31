17db513b36815f9d4d79e154f760fe0f8e899540
nicgirault <nic.girault@gmail.com>
2015-08-31 21:40:56 +0200

    Update 2015-08-31T19:40:56.363Z

--- js/app.js
+++ js/app.js
@@ -1,72 +1,72 @@
 'use strict';
 var app;
 
-app = angular.module('angularParseBoilerplate', ['ng', 'ngResource', 'ui.router', 'ui.bootstrap', 'app.templates', 'Parse', 'angulartics', 'angulartics.google.analytics']);
+app = angular.module('alloArtEtEssai', ['ng', 'ngResource', 'ui.router', 'ui.bootstrap', 'app.templates', 'Parse', 'angulartics', 'angulartics.google.analytics']);
 
 app.config(function($locationProvider, $stateProvider, $urlRouterProvider, ParseProvider) {
   $locationProvider.hashPrefix('!');
-  $stateProvider.state('task', {
+  $stateProvider.state('cinema', {
     url: '/:locale',
-    controller: 'TaskCtrl',
-    templateUrl: 'task.html'
+    controller: 'cinemaCtrl',
+    templateUrl: 'cinema.html'
   });
   $urlRouterProvider.otherwise('/fr');
-  return ParseProvider.initialize("N2xyMRbsrFcBuzq7TXLwieDGM9FzwODEY44LLFOP", "zTAHO7HKWvbV1awq5wQlexRc368lOQtSbmycOi0O");
+  return ParseProvider.initialize("2Y3JhneedL6TfTswvBgPfJbZ0qxQRJHj8jg0GqEU", "w1ek8EuSk7dD8bEBDSN5J8XTyXlGuOgx8mv7q7MD");
 });
 
 app.run(function($rootScope, $state) {
   return $rootScope.$state = $state;
 });
 
-app.controller('TaskCtrl', function($scope, Task) {
-  $scope.addTask = function() {
-    $scope.newTask.save().then(function(task) {
-      return $scope.fetchTasks();
+app.controller('cinemaCtrl', function($scope, Cinema) {
+  $scope.addCinema = function() {
+    $scope.newCinema.save().then(function(cinema) {
+      return $scope.fetchCinemas();
     });
-    return $scope.newTask = new Task;
+    return $scope.newCinema = new Cinema;
   };
-  $scope.removeTask = function(task) {
-    return task.destroy().then(function() {
-      return _.remove($scope.tasks, function(task) {
-        return task.objectId === null;
+  $scope.removeCinema = function(cinema) {
+    return cinema.destroy().then(function() {
+      return _.remove($scope.cinemas, function(cinema) {
+        return cinema.objectId === null;
       });
     });
   };
-  $scope.editingTask = function(task) {
-    return task.editing = true;
+  $scope.editingCinema = function(cinema) {
+    return cinema.editing = true;
   };
-  $scope.editTask = function(task) {
-    task.save();
-    return task.editing = false;
+  $scope.editCinema = function(cinema) {
+    cinema.save();
+    return cinema.editing = false;
   };
-  $scope.cancelEditing = function(task) {
-    task.title = task._cache.title;
-    return task.editing = false;
+  $scope.cancelEditing = function(cinema) {
+    cinema.alloCineId = cinema._cache.alloCineId;
+    return cinema.editing = false;
   };
-  $scope.fetchTasks = function() {
-    return Task.query().then(function(tasks) {
-      return $scope.tasks = tasks;
+  $scope.fetchCinemas = function() {
+    return Cinema.query().then(function(cinemas) {
+      return $scope.cinemas = cinemas;
     });
   };
-  $scope.fetchTasks();
-  return $scope.newTask = new Task;
+  $scope.fetchCinemas();
+  return $scope.newCinema = new Cinema;
 });
 
 var __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-app.factory('Task', function(Parse) {
-  var Task;
-  return Task = (function(_super) {
-    __extends(Task, _super);
+app.factory('Cinema', function(Parse) {
+  var Cinema;
+  return Cinema = (function(_super) {
+    __extends(Cinema, _super);
 
-    function Task() {
-      return Task.__super__.constructor.apply(this, arguments);
+    function Cinema() {
+      return Cinema.__super__.constructor.apply(this, arguments);
     }
 
-    Task.configure("Task", "title");
+    Cinema.configure("Cinema", "alloCineId");
 
-    return Task;
+    return Cinema;
 
   })(Parse.Model);
 });
